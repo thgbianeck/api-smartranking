2398e45f9356358edf275f68d23b7d4c9e2e81b1
Thiago Moreira Bianeck <thgbianeck@gmail.com>
2022-01-29 22:12:43 -0300

    15. Nest e o uso do Inversion of Control Container

---
+++ src/jogadores/jogadores.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class JogadoresService {
+  private jogadores: Jogador[] = [];
+
+  private readonly logger = new Logger(JogadoresService.name);
+
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    this.criar(criarJogadorDto);
+  }
+
+  private criar(criaJogadorDto: CriarJogadorDto): void {
+    const { nome, telefoneCelular, email } = criaJogadorDto;
+
+    const jogador: Jogador = {
+      _id: uuidv4(),
+      nome,
+      telefoneCelular,
+      email,
+      ranking: 'A',
+      posicaoRanking: 1,
+      urlFotoJogador: 'www.google.com.br/foto123.jpg',
+    };
+    this.logger.log(`jogador: ${JSON.stringify(jogador, null, 2)}`);
+    this.jogadores.push(jogador);
+  }
+}
